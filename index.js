da2406b08cff8c972678328c4591bf43a211d3aa
kodelack47 <ousmanatunkara1@gmail.com>
2023-04-27 15:09:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+//comment
+// console.log('Hello World');
+// let name = 'Mosha';
+// console.log(name);
+
+//dont use reserve keywords as var//like if or else
+//should be meaningful
+//cant start with a number
+//cant contain a space oy hyphen
+//are case sensitive
+
+// let firstname = 'Ousmana';
+// let lastname = 'Tunkara';
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+//two types of types
+
+//(*Primitives/value types)-
+//(strings, numbers, boolean, undefined, null)
+// let Name = 'mosh'; //string
+// let age = 20; //number
+// let isApproved = true; //boolean
+// let Firstname = undefined; //undefined
+// let selectedColor = null;
+
+
+//(*Reference types)-
+//(objects, arrays, functions,)
+
+// object like real life properties,
+// let person = {
+//      Name: 'moshy',
+//      age: 30        
+//   }
+//   //no notation *stick to this
+//   person.Name = 'john';
+
+//   //no bracket notation
+//   person['Name'] = 'mary';
+  
+//   console.log(person.Name);
+
+
+// when you have alot objects like a list use arrays - data structure for list of item
+// let selectedColor = ['red','blue'];
+// selectedColor[2] ='green';
+// console.log(selectedColor[0]);
+//if you want access an element in array we use index [] ex- first one is always 0 not one 
+
+
+//fuction statements that performing a task
+// function greet(name, lastname) {
+//     console.log('hello' + name + '' + lastname);
+// }
+
+// greet('john', 'smith');
+
+//calculating a value
+// function square (number) {
+//     return number * number;
+// }
+// console.log(square(2))
+
+
+//two types of java script lang. static and dynamic
+
+//static - when declare a var it is set and cant be changed in the future
+//dynamic - like js the type of var can be changed ex(let name = 'mosh'/ typeof name = string/ name = 30 /typeof name = number)
